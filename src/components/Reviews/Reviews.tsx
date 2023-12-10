99f7f05ba6cc410cffab667396671410f6b712fe
Bogdan-design <bogdanbw@gmail.com>
2023-12-10 22:40:52 +0100

    refactor

--- src/components/Reviews/Reviews.tsx
+++ src/components/Reviews/Reviews.tsx
@@ -1,42 +1,48 @@
 import React from 'react';
-import {SectionStyled} from "../../common/Section.styled";
+import {SectionStyled} from "common/Section.styled";
 import styled from "styled-components";
-import {TitleH3Styled} from "../../common/TitleH3.styled";
-import {ReviewCard} from "../../components/Reviews/ReviewCard";
+import {TitleH3Styled} from "common/TitleH3.styled";
+import {ReviewCard} from "components/Reviews/ReviewCard";
 
 export const Reviews = () => {
     return (
-        <SectionStyled>
-            <BoxStyled>
-                <TitleH3Styled>Reviews</TitleH3Styled>
-                <a>Go to Reviews</a>
-            </BoxStyled>
-            <LineOfProduct>
-                <ReviewCard/>
-                <ReviewCard/>
-                <ReviewCard/>
-                <ReviewCard/>
-            </LineOfProduct>
-        </SectionStyled>
-    );
+            <ReviewsBox>
+                <BoxStyled>
+                    <TitleH3Styled>Reviews</TitleH3Styled>
+                    <a>Go to Reviews</a>
+                </BoxStyled>
+                <LineOfProduct>
+                    <ReviewCard/>
+                    <ReviewCard/>
+                    <ReviewCard/>
+                    <ReviewCard/>
+                </LineOfProduct>
+            </ReviewsBox>
+    )
+        ;
 };
 
-const BoxStyled = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  align-self: stretch;
-
-  a {
-    color: var(--main-primary, #FF4D4D);
-    font-size: 16px;
-    font-weight: 500;
-    line-height: 20px;
-  }
+const ReviewsBox = styled.div`
+    display: flex;
+    justify-content: center;
 `
-const LineOfProduct=styled.div`
-  display: flex;
-  align-items: flex-start;
-  gap: 24px;
-  align-self: stretch;
+
+const BoxStyled = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    align-self: stretch;
+
+    a {
+        color: #FF4D4D;
+        font-size: 16px;
+        font-weight: 500;
+        line-height: 20px;
+    }
+`
+const LineOfProduct = styled.div`
+    display: flex;
+    align-items: flex-start;
+    gap: 24px;
+    align-self: stretch;
 `
